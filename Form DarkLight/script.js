9c80d8059540863b3a8ae072cd0fb5e3d9d51e6b
Patrick Fernando <67243547+PatrickFernandoSouza@users.noreply.github.com>
2023-05-30 21:21:48 -0400

    Add files via upload

---
+++ Form DarkLight/script.js
@@ -0,0 +1,17 @@
+let mode = document.getElementById('icon');
+
+mode.addEventListener('click', () => {
+    let form = document.getElementById('login_form')
+    if (mode.classList.contains('fa-moon')) {
+        mode.classList.remove('fa-moon');
+        mode.classList.add('fa-sun');
+
+        form.classList.add('dark')
+        return;
+    }
+
+    mode.classList.add('fa-moon');
+    mode.classList.remove('fa-sun');
+    form.classList.remove('dark')
+
+});
